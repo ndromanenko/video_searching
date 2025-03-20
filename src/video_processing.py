cca372f711058c504de63008a674c06d8b2050bd
ndromanenko <96349390+ndromanenko@users.noreply.github.com>
2025-03-20 23:24:53 +0300

    Delete src directory

--- src/video_processing.py
+++
@@ -1,7 +0,0 @@
-from run import process_video
-from src.utils import transcribe
-
-def process_and_transcribe(video_path, temp_dir, model):
-    process_video(video_path, temp_dir)
-    transcribed_data = transcribe(model, temp_dir)
-    return transcribed_data
